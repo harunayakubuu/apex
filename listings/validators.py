c310afacadcc398de227c0e6a6bff0f07aa8d7fc
Haruna Yakubu <harunayakubuu@gmail.com>
2023-07-27 09:47:20 +0100

    Updated validators

--- listings/validators.py
+++ listings/validators.py
@@ -4,15 +4,15 @@ from django.core.exceptions import ValidationError
 def validate_picture_size(value):
     picutre_size = value.size
 
-    if picutre_size > 102400: # 50 Kilobyte - 1kb = 1024
-        raise ValidationError(f'File too large. Maximum size allowed is 100kb.')
+    if picutre_size > 1024000:
+        raise ValidationError(f'File too large. Maximum size allowed is 500kb.')
     else:
         return value
         
 def validate_floor_design_size(value):
     floor_design_size = value.size
 
-    if floor_design_size > 51200: # 50 Kilobyte - 1kb = 1024
+    if floor_design_size > 1024000:
         floor_design_size = floor_design_size/1024
         raise ValidationError(f'File too large. Maximum size allowed is 50kb.')
     else:
@@ -34,7 +34,7 @@ def validate_picture_dimension(width=1200, height=800):
 
 
 def validate_video_size(value):
-    if value.file.content_type == 'application/video':
+    if value.file.content_type == 'application/mp4':
         video_size = value.size
         if video_size > 5242880:#10485760: # 10MB
         
